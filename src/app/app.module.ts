24b73a0b0c11c335e1b1f1a5ce2fcd80d8088a19
Murilo Felipin <murilo-d@hotmail.com>
2023-05-28 19:03:00 -0300

    Criação de dois modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
